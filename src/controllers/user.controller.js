56ef117b199b3c7940b79f1b847eaafb6bb0a64e
pavank3673 <pavank3673@outlook.com>
2024-11-19 16:17:27 +0530

    [Pavan Kumar]Added:JWT

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,8 @@
 import HttpStatus from 'http-status-codes';
 import * as UserService from '../services/user.service';
-import bcrypt from 'bcrypt';
 
 export const registerUser = async (req, res, next) => {
   try {
-    req.body.password = await bcrypt.hash(req.body.password, 10);
     const data = await UserService.registerUser(req.body);
     res.status(HttpStatus.CREATED).json({
       code: HttpStatus.CREATED,
@@ -18,25 +16,16 @@ export const registerUser = async (req, res, next) => {
 
 export const loginUser = async (req, res, next) => {
   try {
-    const data = await UserService.getUserByEmail(req.body.email);
-    let isEqual = false;
-    if (data != null) {
-      isEqual = await bcrypt.compare(req.body.password, data.password);
-    }
-    if (isEqual) {
-      res.status(HttpStatus.OK).json({
-        code: HttpStatus.OK,
-        data: data,
-        message: 'User logged in successfully'
-      });
-    } else {
-      res.status(HttpStatus.NOT_FOUND).json({
-        code: HttpStatus.NOT_FOUND,
-        data: [],
-        message: 'Invalid credentials'
-      });
-    }
+    const token = await UserService.getUserByEmail(req.body);
+    res.status(HttpStatus.OK).json({
+      code: HttpStatus.OK,
+      data: token,
+      message: 'User logged in successfully'
+    });
   } catch (error) {
-    next(error);
+    res.status(HttpStatus.BAD_REQUEST).json({
+      code: HttpStatus.BAD_REQUEST,
+      message: `${error}`
+    });
   }
 };
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,12 +1,28 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import sequelize, { DataTypes } from '../config/database';
 const User = require('../models/user')(sequelize, DataTypes);
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 export const registerUser = async (body) => {
+  body.password = await bcrypt.hash(body.password, 10);
   const data = await User.create(body);
   return data;
 };
 
-export const getUserByEmail = async (email) => {
-  const data = await User.findOne({ where: { email: email } });
-  return data;
+export const getUserByEmail = async (req) => {
+  const data = await User.findOne({ where: { email: req.email } });
+  if (data != null) {
+    if (await bcrypt.compare(req.password, data.password)) {
+      return jwt.sign(
+        { id: data.id, email: data.email },
+        process.env.ACCESS_TOKEN_SECRET
+      );
+    } else {
+      throw new Error('Invalid Password');
+    }
+  } else {
+    throw new Error('Invalid Email');
+  }
 };
