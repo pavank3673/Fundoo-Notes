5e351b35b9b66b8072212a72917886b00b8f93a0
pavank3673 <pavank3673@outlook.com>
2024-11-26 16:16:23 +0530

    [Pavan Kumar]Added:swagger api docs feature

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ dotenv.config();
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../src/swagger/openapi.json';
 
 import routes from './routes';
 import {
@@ -26,6 +28,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan('combined', { stream: logStream }));
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(`/api/${api_version}`, routes());
 app.use(appErrorHandler);
 app.use(genericErrorHandler);
